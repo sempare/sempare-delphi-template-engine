a0cbd5db614070d5a573e035bd64587e35dee7b1
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2024-05-27 11:23:12 +0200

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,3 @@
-rsvars.bat
-C:\windows\Microsoft.NET\Framework\v4.0.30319\msbuild Sempare.Template.Tester.dproj /t:Build /p:Config=%~1 /p:Platform=Win32
-
 set PATH=%PATH%;C:\windows\Microsoft.NET\Framework\v4.0.30319
 set PATH=%PATH%;C:\Program Files (x86)\Embarcadero\Studio\23.0\bin
 call rsvars.bat
