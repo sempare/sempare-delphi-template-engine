dd2251f291455fecb37fda87b13d20fa7be4ac27
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2024-05-27 10:59:33 +0200

    test

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,6 @@
-rsvars.bat
-C:\windows\Microsoft.NET\Framework\v4.0.30319\msbuild Sempare.Template.Tester.dproj /t:Build /p:Config=%~1 /p:Platform=Win32
+set PATH=%PATH%;C:\windows\Microsoft.NET\Framework\v4.0.30319
+set PATH=%PATH%;C:\Program Files (x86)\Embarcadero\Studio\23.0\bin
+call rsvars.bat
+cd ..
+msbuild Sempare.Template.Tester.dproj /t:Clean /p:Config=%~1 /p:Platform=%~2
+msbuild Sempare.Template.Tester.dproj /t:Build /p:Config=%~1 /p:Platform=%~2
