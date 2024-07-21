c28152e6572b586cb63fce25389454891c10de03
Conrad Vermeulen <conrad.vermeulen@gmail.com>
2024-07-21 13:42:32 +0100

    Use JUnit format for tests to work with github

--- Sempare.Template.Tester.dpr
+++ Sempare.Template.Tester.dpr
@@ -35,14 +35,13 @@ program Sempare.Template.Tester;
 {$IFNDEF TESTINSIGHT}
 {$APPTYPE CONSOLE}
 {$ENDIF}{$STRONGLINKTYPES ON}
-
 uses
   System.SysUtils,
-{$IFDEF TESTINSIGHT}
+  {$IFDEF TESTINSIGHT}
   TestInsight.DUnitX,
-{$ENDIF }
+  {$ENDIF }
   DUnitX.Loggers.Console,
-  DUnitX.Loggers.XML.NUnit,
+  DUnitX.Loggers.Xml.JUnit,
   DUnitX.TestFramework,
   Sempare.Template.TestIf in 'tests\Sempare.Template.TestIf.pas',
   Sempare.Template.TestContext in 'tests\Sempare.Template.TestContext.pas',
@@ -76,37 +75,36 @@ begin
   exit;
 {$ENDIF}
   try
-    // Check command line options, will exit if invalid
+    //Check command line options, will exit if invalid
     TDUnitX.CheckCommandLine;
-    // Create the test runner
+    //Create the test runner
     runner := TDUnitX.CreateRunner;
-    // Tell the runner to use RTTI to find Fixtures
-    runner.UseRTTI := true;
-    // tell the runner how we will log things
-    // Log to the console window
+    //Tell the runner to use RTTI to find Fixtures
+    runner.UseRTTI := True;
+    //tell the runner how we will log things
+    //Log to the console window
     logger := TDUnitXConsoleLogger.Create(true);
     runner.AddLogger(logger);
     //Generate an NUnit compatible XML File
-    nunitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
+    nunitLogger := TDUnitXXMLJUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
     runner.AddLogger(nunitLogger);
     runner.FailsOnNoAsserts := False; //When true, Assertions must be made during tests;
 
-    // Run tests
+    //Run tests
     results := runner.Execute;
     if not results.AllPassed then
       System.ExitCode := EXIT_ERRORS;
 
-{$IFNDEF CI}
-    // We don't want this happening when running under CI.
+    {$IFNDEF CI}
+    //We don't want this happening when running under CI.
     if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
     begin
       System.Write('Done.. press <Enter> key to quit.');
       System.Readln;
     end;
-{$ENDIF}
+    {$ENDIF}
   except
     on E: Exception do
       System.Writeln(E.ClassName, ': ', E.Message);
   end;
-
 end.
